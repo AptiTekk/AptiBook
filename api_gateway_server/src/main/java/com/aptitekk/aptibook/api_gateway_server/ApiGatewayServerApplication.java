d3fe2ef800cba3d7c3008b79ae21e54d6db0b93f
Mitch Talmadge <mitch.talmadge@aptitekk.com>
2018-05-19 20:47:13 -0600

    Create framework for API Gateway Server

---
+++ api_gateway_server/src/main/java/com/aptitekk/aptibook/api_gateway_server/ApiGatewayServerApplication.java
@@ -0,0 +1,12 @@
+package com.aptitekk.aptibook.api_gateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServerApplication.class, args);
+    }
+}
