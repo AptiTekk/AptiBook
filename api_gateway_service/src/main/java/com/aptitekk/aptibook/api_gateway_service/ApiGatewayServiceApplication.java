091eb3828b407f1a3f90b3d3a9a090c61c2e465d
Mitch Talmadge <mitch.talmadge@aptitekk.com>
2018-05-21 17:34:02 -0600

    Rename "server" to "service". Add Dockerfiles.

---
+++ api_gateway_service/src/main/java/com/aptitekk/aptibook/api_gateway_service/ApiGatewayServiceApplication.java
@@ -0,0 +1,12 @@
+package com.aptitekk.aptibook.api_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
+    }
+}
